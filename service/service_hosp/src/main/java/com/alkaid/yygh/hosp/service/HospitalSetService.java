5d125ca7204d73a01d9686f52b74bf625471d7b2
Alkaid <1125097352@qq.com>
2021-08-03 18:38:43 +0800

    d1 service模块

---
+++ service/service_hosp/src/main/java/com/alkaid/yygh/hosp/service/HospitalSetService.java
@@ -0,0 +1,16 @@
+package com.alkaid.yygh.hosp.service;
+
+import com.alkaid.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * Created with IntelliJ IDEA.2020.2.3
+ *
+ * @Auther: Alkaid
+ * @Date: 2021-08-03 16:28
+ * @ClassName HospitalSetService
+ * @Description:
+ */
+
+public interface HospitalSetService extends IService<HospitalSet> {
+}
---
+++ service/service_hosp/src/main/java/com/alkaid/yygh/hosp/service/impl/HospitalSetServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alkaid.yygh.hosp.service.impl;
+
+import com.alkaid.yygh.hosp.mapper.HospitalSetMapper;
+import com.alkaid.yygh.hosp.service.HospitalSetService;
+import com.alkaid.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created with IntelliJ IDEA.2020.2.3
+ *
+ * @Auther: Alkaid
+ * @Date: 2021-08-03 16:29
+ * @ClassName HospitalSetServiceImpl
+ * @Description:
+ */
+@Service
+public class HospitalSetServiceImpl extends ServiceImpl<HospitalSetMapper, HospitalSet> implements HospitalSetService {
+
+}
