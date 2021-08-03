53ec9cf2f25d180c2331a25227b1dee2778d712e
Alkaid <1125097352@qq.com>
2021-08-03 18:38:56 +0800

    d1 mapper模块

---
+++ service/service_hosp/src/main/java/com/alkaid/yygh/hosp/mapper/HospitalSetMapper.java
@@ -0,0 +1,16 @@
+package com.alkaid.yygh.hosp.mapper;
+
+import com.alkaid.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created with IntelliJ IDEA.2020.2.3
+ *
+ * @Auther: Alkaid
+ * @Date: 2021-08-03 16:24
+ * @ClassName HospitalSetMapper
+ * @Description:
+ */
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+}
