7b1bb4e41e1359a6cda3069384da5a2d934b4672
Alkaid <1125097352@qq.com>
2021-08-06 15:17:17 +0800

    分页插件

--- service/service_hosp/src/main/java/com/alkaid/yygh/hosp/config/HospConfig.java
+++ service/service_hosp/src/main/java/com/alkaid/yygh/hosp/config/HospConfig.java
@@ -1,6 +1,8 @@
 package com.alkaid.yygh.hosp.config;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -14,4 +16,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("com.alkaid.yygh.hosp.mapper")
 public class HospConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
 }
