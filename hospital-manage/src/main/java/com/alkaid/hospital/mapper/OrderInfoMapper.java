8acbcb50cdc9d3ff6e5def537405a7a32e31ecf0
Alkaid <1125097352@qq.com>
2021-08-07 16:58:19 +0800

    医院模块

---
+++ hospital-manage/src/main/java/com/alkaid/hospital/mapper/OrderInfoMapper.java
@@ -0,0 +1,10 @@
+package com.alkaid.hospital.mapper;
+
+import com.alkaid.hospital.model.OrderInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
+
+}
