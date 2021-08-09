baadcb41f25bfbed1c14d9e098faa828c3696179
Alkaid <1125097352@qq.com>
2021-08-09 19:25:01 +0800

    增加nacos和feign注解

--- service/service_hosp/src/main/java/com/alkaid/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/alkaid/yygh/hosp/ServiceHospApplication.java
@@ -2,8 +2,9 @@ package com.alkaid.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 /**
  * Created with IntelliJ IDEA.2020.2.3
@@ -15,6 +16,8 @@ import org.springframework.stereotype.Component;
  */
 @SpringBootApplication
 @ComponentScan(basePackages = "com.alkaid")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.alkaid")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class,args);
