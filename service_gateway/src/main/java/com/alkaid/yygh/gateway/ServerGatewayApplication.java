8b969225431e257a0e0b2783323b0accd643656f
Alkaid <1125097352@qq.com>
2021-08-11 17:26:56 +0800

    配置gateway解决跨域问题

---
+++ service_gateway/src/main/java/com/alkaid/yygh/gateway/ServerGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.alkaid.yygh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.2020.2.3
+ *
+ * @Auther: Alkaid
+ * @Date: 2021-08-11 16:07
+ * @ClassName ServerGatewayApplication
+ * @Description:
+ */
+@SpringBootApplication
+public class ServerGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGatewayApplication.class,args);
+    }
+}
