8e672ed0080828ea30d3c7a6c2a580f3610ad24d
Alkaid <1125097352@qq.com>
2021-08-13 21:58:54 +0800

    用户信息mapper

---
+++ service/service_user/src/main/java/com/alkaid/yygh/user/mapper/UserInfoMapper.java
@@ -0,0 +1,15 @@
+package com.alkaid.yygh.user.mapper;
+
+import com.alkaid.yygh.model.user.UserInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * Created with IntelliJ IDEA.2020.2.3
+ * @Auther: Alkaid
+ * @Date: 2021-08-13 14:20
+ * @ClassName UserInfoMapper
+ * @Description:
+ */
+
+public interface UserInfoMapper extends BaseMapper<UserInfo> {
+}
