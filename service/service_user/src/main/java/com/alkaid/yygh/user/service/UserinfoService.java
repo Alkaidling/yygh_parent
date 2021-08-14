f7b9987bff8c85444c1f4a42173355e84f1abbfa
Alkaid <1125097352@qq.com>
2021-08-14 21:27:18 +0800

    微信扫码登录

--- service/service_user/src/main/java/com/alkaid/yygh/user/service/UserinfoService.java
+++ service/service_user/src/main/java/com/alkaid/yygh/user/service/UserinfoService.java
@@ -1,6 +1,9 @@
 package com.alkaid.yygh.user.service;
 
+import com.alkaid.yygh.model.user.UserInfo;
 import com.alkaid.yygh.vo.user.LoginVo;
+import com.alkaid.yygh.vo.user.UserAuthVo;
+import com.baomidou.mybatisplus.extension.service.IService;
 
 import java.util.Map;
 
@@ -13,7 +16,11 @@ import java.util.Map;
  * @Description:
  */
 
-public interface UserinfoService {
+public interface UserinfoService extends IService<UserInfo> {
     //用户手机号登录接口
     Map<String, Object> loginUser(LoginVo loginVo);
+    //根据openid判断数据库是否存在微信扫码人的信息
+    UserInfo selectWxInfoOpenid(String openid);
+    //用户认证
+    void userAuth(Long userId, UserAuthVo userAuthVo);
 }
--- service/service_user/src/main/java/com/alkaid/yygh/user/service/impl/UserInfoServiceImpl.java
+++ service/service_user/src/main/java/com/alkaid/yygh/user/service/impl/UserInfoServiceImpl.java
@@ -3,10 +3,12 @@ package com.alkaid.yygh.user.service.impl;
 import com.alkaid.yygh.common.exception.YyghException;
 import com.alkaid.yygh.common.helper.JwtHelper;
 import com.alkaid.yygh.common.result.ResultCodeEnum;
+import com.alkaid.yygh.enums.AuthStatusEnum;
 import com.alkaid.yygh.model.user.UserInfo;
 import com.alkaid.yygh.user.mapper.UserInfoMapper;
 import com.alkaid.yygh.user.service.UserinfoService;
 import com.alkaid.yygh.vo.user.LoginVo;
+import com.alkaid.yygh.vo.user.UserAuthVo;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
@@ -44,18 +46,34 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
             throw new YyghException(ResultCodeEnum.CODE_ERROR);
         }
 
-        //4、判断是否是第一次登录，根据手机号查询数据库，不存在相同手机号则为第一次登录
-        QueryWrapper<UserInfo> wrapper = new QueryWrapper<>();
-        wrapper.eq("phone",phone);
-        UserInfo userInfo = baseMapper.selectOne(wrapper);
+        //绑定手机号码
+        UserInfo userInfo = null;
+        //userInfo!=null 说明微信扫码之后绑定手机再登录
+        if(!StringUtils.isEmpty(loginVo.getOpenid())) {
+            userInfo = this.selectWxInfoOpenid(loginVo.getOpenid());
+            if(null != userInfo) {
+                userInfo.setPhone(loginVo.getPhone());
+                this.updateById(userInfo);
+            } else {
+                throw new YyghException(ResultCodeEnum.DATA_ERROR);
+            }
+        }
 
-        //5、第一次登录，添加信息到数据库
-        if (userInfo == null) {
-            userInfo = new UserInfo();
-            userInfo.setName("");
-            userInfo.setPhone(phone);
-            userInfo.setStatus(1);
-            baseMapper.insert(userInfo);
+        //userInfo=null 说明手机直接登录
+        if(null == userInfo) {
+            //4、判断是否是第一次登录，根据手机号查询数据库，不存在相同手机号则为第一次登录
+            QueryWrapper<UserInfo> wrapper = new QueryWrapper<>();
+            wrapper.eq("phone",phone);
+            userInfo = baseMapper.selectOne(wrapper);
+
+            //5、第一次登录，添加信息到数据库
+            if (userInfo == null) {
+                userInfo = new UserInfo();
+                userInfo.setName("");
+                userInfo.setPhone(phone);
+                userInfo.setStatus(1);
+                baseMapper.insert(userInfo);
+            }
         }
 
         //校验是否被禁用
@@ -80,5 +98,30 @@ public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> i
         map.put("token", token);
 
         return map;
+    }
+
+    //根据openid判断数据库是否存在微信扫码人的信息
+    @Override
+    public UserInfo selectWxInfoOpenid(String openid) {
+        QueryWrapper<UserInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("openid",openid);
+        UserInfo userInfo = baseMapper.selectOne(queryWrapper);
+
+        return userInfo;
+    }
+
+    //用户认证
+    @Override
+    public void userAuth(Long userId, UserAuthVo userAuthVo) {
+        //根据用户id查询用户信息
+        UserInfo userInfo = baseMapper.selectById(userId);
+        //设置认证信息
+        userInfo.setName(userAuthVo.getName());
+        userInfo.setCertificatesType(userAuthVo.getCertificatesType());
+        userInfo.setCertificatesNo(userAuthVo.getCertificatesNo());
+        userInfo.setCertificatesUrl(userAuthVo.getCertificatesUrl());
+        userInfo.setAuthStatus(AuthStatusEnum.AUTH_RUN.getStatus());
+        //进行信息更新
+        baseMapper.updateById(userInfo);
     }
 }
