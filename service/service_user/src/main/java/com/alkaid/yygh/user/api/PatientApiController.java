f19b2980cc0621ca86f25181c1efe28cb7668e64
Alkaid <1125097352@qq.com>
2021-08-17 10:29:12 +0800

    根据就诊人id获取就诊人信息

--- service/service_user/src/main/java/com/alkaid/yygh/user/api/PatientApiController.java
+++ service/service_user/src/main/java/com/alkaid/yygh/user/api/PatientApiController.java
@@ -63,4 +63,11 @@ public class PatientApiController {
         patientService.removeById(id);
         return Result.ok();
     }
+
+    //根据就诊人id获取就诊人信息
+    @GetMapping("inner/get/{id}")
+    public Patient getPatientOrder(@PathVariable Long id){
+        Patient patient = patientService.getPatientId(id);
+        return patient;
+    }
 }
