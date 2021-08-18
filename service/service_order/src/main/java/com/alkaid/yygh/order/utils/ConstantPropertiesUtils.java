70196aadc4b45bd7ae48384c889fc6c642f10dee
Alkaid <1125097352@qq.com>
2021-08-18 18:40:19 +0800

    微信支付相关工具类

---
+++ service/service_order/src/main/java/com/alkaid/yygh/order/utils/ConstantPropertiesUtils.java
@@ -0,0 +1,41 @@
+package com.alkaid.yygh.order.utils;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.2020.2.3
+ *
+ * @Auther: Alkaid
+ * @Date: 2021-08-18 15:23
+ * @ClassName ConstantPropertiesUtils
+ * @Description:
+ */
+@Component
+public class ConstantPropertiesUtils implements InitializingBean {
+
+    @Value("${weixin.appid}")
+    private String appid;
+
+    @Value("${weixin.partner}")
+    private String partner;
+
+    @Value("${weixin.partnerkey}")
+    private String partnerkey;
+
+    @Value("${weixin.cert}")
+    private String cert;
+
+    public static String APPID;
+    public static String PARTNER;
+    public static String PARTNERKEY;
+    public static String CERT;
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        APPID = appid;
+        PARTNER = partner;
+        PARTNERKEY = partnerkey;
+        CERT = cert;
+    }
+}
